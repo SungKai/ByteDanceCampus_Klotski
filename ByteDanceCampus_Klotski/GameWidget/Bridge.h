3d372eb9490740048916861f7928a6e7eadd9438
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2022-08-18 23:14:06 +0800

    [Featuer]Add widget.

---
+++ ByteDanceCampus_Klotski/GameWidget/Bridge.h
@@ -0,0 +1,14 @@
+//
+//  Bridge.h
+//  ByteDanceCampus_Klotski
+//
+//  Created by Magic Keegan on 8/18/22.
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+#import "TeamIntroduceView.h"
+#import  "KlotskiHeader.h"
+
+#endif /* Bridge_h */
